12e0f05ecabdf7a2d45fdc4fd1d513914fad89e8
peixed351 <peixed351@gmail.com>
2024-03-24 17:29:04 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "VisDrone2019-DET"
 PROJECT_NAME_FULL: str = "VisDrone2019-DET Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
